0e01dd8a0e0ddedc219bf26bd13d547e5ebccbb6
Rozhdestvenskiy Andrey <rozhdestvenskiyam@gmail.com>
2024-03-02 16:38:38 +0300

    supplier init

---
+++ sipplier/src/main/java/ru/rozhdestvenskiy/sipplier/SipplierApplication.java
@@ -0,0 +1,13 @@
+package ru.rozhdestvenskiy.sipplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipplierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SipplierApplication.class, args);
+    }
+
+}
---
+++ sipplier/src/test/java/ru/rozhdestvenskiy/sipplier/SipplierApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rozhdestvenskiy.sipplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipplierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
